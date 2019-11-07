cfa7db7845c918a01bee7f10b3b8a359c7ca32c1
Dipen Desai <dnd.devops@gmail.com>
2019-11-06 21:13:37 -0800

    First code details

---
+++ src/test/java/com/dd/aws/ebs1/Ebs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dd.aws.ebs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ebs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
